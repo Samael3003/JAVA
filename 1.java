d572fea99a950228a3f605dc8937c0cba880204a
Samael3003 <103866475+Samael3003@users.noreply.github.com>
2023-05-24 18:47:26 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,52 @@
+# Program to remove all repeated elements from an array
+
+
+import java.util.*;
+
+public class RemoveDuplicateInArrayExample {
+    public static int removeDuplicateElements(int arr[], int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int[] temp = new int[n];
+        int j = 0;
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[n - 1];
+        // Changing original array
+        for (int i = 0; i < j; i++) {
+            arr[i] = temp[i];
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = input.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+        input.close();
+
+        System.out.println("\nOriginal Array:");
+        printArray(arr, size);
+
+        int length = removeDuplicateElements(arr, size);
+
+        System.out.println("\nArray after removing duplicates:");
+        printArray(arr, length);
+    }
+
+    public static void printArray(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
